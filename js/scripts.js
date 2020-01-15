ca925a27a0427fce51ba8d59abeb22f35e5ec1d6
David Boedigheimer <stealth259@gmail.com>
2020-01-15 15:51:07 -0800

    Add start of bankaccount in java.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function BankAccount(accountName, initialAmount, depositAmount, withdrawAmount) {
+  this.accountName = accountName;
+  this.initialAmount = initialAmount;
+  this.depositAmount = depositAmount;
+  this.withdrawAmount = withdrawAmount;
+}
+
+BankAccount.prototype.addDeposit = function() {
+
+}
+
+BankAccount.prototype.subWithdraw = function() {
+
+}
+
+// user logic

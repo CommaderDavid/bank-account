b52a5be51bf628fd4731adc089e6ac734ac4da0f
David Boedigheimer <stealth259@gmail.com>
2020-01-16 12:36:52 -0800

    Add both submit functions.

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,26 @@ function BankAccount(accountName, initialAmount, depositAmount, withdrawAmount) 
 }
 
 BankAccount.prototype.addDeposit = function() {
-
+  this.initialAmount += this.depositAmount
 }
 
 BankAccount.prototype.subWithdraw = function() {
-
+  this.initialAmount -= this.withdrawAmount
 }
 
 // user logic
+$(document).ready(function() {
+  $("form#account-setup").submit(function(e) {
+    e.preventDefault();
+    var userInputName = $("input#name").val();
+    var firstDeposit = $("input#first-deposit").val();
+
+  })
+
+  $("form#account-management").submit(function(e) {
+    e.preventDefault();
+    var deposit = $("input#deposit").val();
+    var withdraw = $("input#withdraw").val();
+    
+  })
+})

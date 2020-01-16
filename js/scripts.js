14f2d06bcd523dc534d63be8fb943737c8aca7ff
David Boedigheimer <stealth259@gmail.com>
2020-01-16 14:19:28 -0800

    Add display to function, start display options for main page, and add step to all number inputs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
-function BankAccount(accountName, initialAmount, depositAmount, withdrawAmount) {
+function BankAccount(accountName, initialAmount) {
   this.accountName = accountName;
   this.initialAmount = initialAmount;
-  this.depositAmount = depositAmount;
-  this.withdrawAmount = withdrawAmount;
 }
 
-BankAccount.prototype.addDeposit = function() {
-  this.initialAmount += this.depositAmount
+BankAccount.prototype.addDeposit = function(depositAmount) {
+  this.initialAmount += depositAmount
+  return this.initialAmount;
 }
 
-BankAccount.prototype.subWithdraw = function() {
-  this.initialAmount -= this.withdrawAmount
+BankAccount.prototype.subWithdraw = function(withdrawAmount) {
+  this.initialAmount -= withdrawAmount
+  return this.initialAmount;
 }
 
 // user logic
@@ -20,12 +20,19 @@ $(document).ready(function() {
     var userInputName = $("input#name").val();
     var firstDeposit = $("input#first-deposit").val();
 
+    $("input#name").val("");
+    $("input#first-deposit").val("");
+
+    var personalAccount = new BankAccount(userInputName, firstDeposit);
+    console.log(firstDeposit);
+    $("#amount").append(personalAccount.initialAmount);
+    $("#user-account").show();
   })
 
   $("form#account-management").submit(function(e) {
     e.preventDefault();
     var deposit = $("input#deposit").val();
     var withdraw = $("input#withdraw").val();
-    
+
   })
 })
